b9035419898118854d3b0bff3a3d06504de40e3d
Sadhanatamtam <tamtamsadhana@gmail.com>
2024-06-25 15:27:23 +0530

    intial commit

---
+++ src/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class HelloWorld {
+	
+	public void welcomeMessage() {
+		System.out.println("Hello everyone, Welcome to devops training!!!");
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+		obj.welcomeMessage();
+	}
+
+}
